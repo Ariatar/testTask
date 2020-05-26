ab2b1c2a3adfe97d065d42121eab6e64a7528a67
Ariatar <hellgate9292@gmail.com>
2020-05-26 17:58:20 +0300

    first commit

---
+++ precache-manifest.a53f9d30fd207d219bc260e47fbc0df7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a31e1ae2c7d5aafdf9",
+    "url": "/testTask/css/about.f954b0b2.css"
+  },
+  {
+    "revision": "cb9952375ec9deb11de8",
+    "url": "/testTask/css/chunk-vendors.1bda9424.css"
+  },
+  {
+    "revision": "e6e93df4af7b7bc8d86a807d8de9ab6c",
+    "url": "/testTask/img/Yo.e6e93df4.jpg"
+  },
+  {
+    "revision": "3b1c01c046bbe9ea68210db2ef756417",
+    "url": "/testTask/index.html"
+  },
+  {
+    "revision": "93a31e1ae2c7d5aafdf9",
+    "url": "/testTask/js/about.c3a49c3a.js"
+  },
+  {
+    "revision": "b954cf41ca7adbe50626",
+    "url": "/testTask/js/app.4589510c.js"
+  },
+  {
+    "revision": "cb9952375ec9deb11de8",
+    "url": "/testTask/js/chunk-vendors.1d7852c9.js"
+  },
+  {
+    "revision": "2e310a9a565a111a8626ab5537a6a54a",
+    "url": "/testTask/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testTask/robots.txt"
+  }
+]);
